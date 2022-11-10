3d822026420ea0283f4df4355e16550f5005c22e
Pavel Jiranek <pavel.jiranek@gooddata.com>
2022-11-11 00:40:03 +0100

    feat(api): add unlink-plugin endpoint

---
+++ src/pages/api/unlink-plugin.ts
@@ -0,0 +1,36 @@
+import { execSync } from "child_process";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type UnlinkPluginRequestBody = {
+    gdcUsername: string;
+    gdcPassword: string;
+    workspaceId: string;
+    dashboardId: string;
+    backendUrl: string;
+    pluginId: string;
+    backendType?: "bear" | "tiger";
+};
+
+interface AddPluginRequest extends NextApiRequest {
+    body: UnlinkPluginRequestBody;
+}
+
+export default function unlinkPlugin(req: AddPluginRequest, res: NextApiResponse) {
+    const {
+        gdcUsername,
+        gdcPassword,
+        pluginId,
+        backendUrl,
+        workspaceId,
+        dashboardId,
+        backendType = "bear",
+    } = req.body;
+
+    const output = execSync(
+        `cross-env GDC_USERNAME=${gdcUsername} GDC_PASSWORD=${gdcPassword}  npx @gooddata/plugin-toolkit dashboard-plugin unlink "${pluginId}" --backend ${backendType} --hostname "${backendUrl}" --workspace-id "${workspaceId}" --dashboard-id "${dashboardId}"`,
+        { encoding: "utf-8" },
+    );
+
+    res.status(200).json(JSON.stringify(output));
+}
